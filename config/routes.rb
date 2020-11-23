4e4b247550e3bcdc787e3e7b21acb93349c03746
keita1988 <keita12may@gmail.com>
2020-11-23 21:56:23 +0900

    コンソールログ仮実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
-  resources :tweets
+  resources :tweets do
     collection do
       get 'search'
     end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
