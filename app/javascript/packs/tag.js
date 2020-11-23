59d3233a1db0d262a1ebedc9305e9eb2649f70e8
keita1988 <keita12may@gmail.com>
2020-11-23 22:07:28 +0900

    jsの実装

--- app/javascript/packs/tag.js
+++ app/javascript/packs/tag.js
@@ -1,1 +1,23 @@
-console.log("読み込み完了");
+if (location.pathname.match("tweets/new")){
+  document.addEventListener("DOMContentLoaded", () => {
+    const inputElement = document.getElementById("tweets_tag_name");
+    inputElement.addEventListener("keyup", () => {
+      const keyword = document.getElementById("tweets_tag_name").value;
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `search/?keyword=${keyword}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      XHR.onload = () => {
+        const tagName = XHR.response.keyword;
+        const searchResult = document.getElementById("search-result");
+        tagName.forEach((tag) => {
+          const childElement = document.createElement("div");
+          childElement.setAttribute("class", "child");
+          childElement.setAttribute("id", tag.id);
+          childElement.innerHTML = tag.name;
+          searchResult.appendChild(childElement);
+        });
+      };
+    });
+  });
+};
