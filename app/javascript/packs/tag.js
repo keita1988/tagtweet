664514b381a01aa25277d3fffcdbcd3e67ee8a17
keita1988 <keita12may@gmail.com>
2020-11-23 22:14:21 +0900

    クリック実装

--- app/javascript/packs/tag.js
+++ app/javascript/packs/tag.js
@@ -10,12 +10,17 @@ if (location.pathname.match("tweets/new")){
       XHR.onload = () => {
         const tagName = XHR.response.keyword;
         const searchResult = document.getElementById("search-result");
+        searchResult.innerHTML = "";
         tagName.forEach((tag) => {
           const childElement = document.createElement("div");
           childElement.setAttribute("class", "child");
           childElement.setAttribute("id", tag.id);
           childElement.innerHTML = tag.name;
           searchResult.appendChild(childElement);
+          const clickElement = document.getElementById(tag.id);
+          clickElement.addEventListener("click", () => {
+            document.getElementById("tweets_tag_name").value = clickElement.textContent;
+            clickElement.remove();
         });
       };
     });
